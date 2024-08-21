6bd96571f89f3d818749080f132fa0c414e74943
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2024-08-21 16:42:20 +0700

    feat(mini-apps): showing mini-apps card on landing page

--- apps/web/components/niwi-mini-apps/niwi-mini-apps-list.tsx
+++ apps/web/components/niwi-mini-apps/niwi-mini-apps-list.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import Image from "next/image";
-import { cn } from "@/libs/utils";
 import { lancelotFont } from "@/libs/font/font-helper";
+import { cn } from "@/libs/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 const MiniAppCard = ({
   title,
@@ -30,6 +30,39 @@ const MiniAppCard = ({
 };
 
 function NiwiMiniAppsList() {
+  const list = [
+    {
+      title: "Todo List",
+      body: "Basic Todo List Application",
+      image: "/images/mini-apps/todo-list.jpg",
+      link: "/mini-apps/todo",
+    },
+    {
+      title: "Preview Url",
+      body: "When you paste url, it'll show ss.",
+      image: "/images/mini-apps/screenshot.jpg",
+      link: "/mini-apps/screenshot-url",
+    },
+    {
+      title: "Pomodoro",
+      body: "Timer for study or progress.",
+      image: "/images/mini-apps/pomodoro.jpg",
+      link: "/mini-apps/pomodoro",
+    },
+    {
+      title: "Tic Tac Toe",
+      body: "Tic Tac Toe Game for Having fun.",
+      image: "/images/mini-apps/tic-tac-toe.jpg",
+      link: "/mini-apps/tic-tac-toe",
+    },
+    // {
+    //   title: "Coming Soon",
+    //   body: "Stay tuned for the next mini-apps.",
+    //   image: "/images/niwi-blog.png",
+    //   link: "/mini-apps",
+    // },
+  ] as const;
+
   return (
     <section className="max-w-[760px] mx-auto px-[20px] mb-8">
       <div className="text-center">
@@ -41,33 +74,19 @@ function NiwiMiniAppsList() {
         >
           Mini-Apps
         </h1>
+
+        <p className="my-2"> This is for learning purposes, provided by Niwi-Starter.</p>
       </div>
 
       <div className="flex flex-row flex-wrap gap-[25px] my-5 justify-center">
-        <MiniAppCard
-          title="Todo List"
-          body="Basic Todo List Application"
-          image="/images/mini-apps/todo-list.gif"
-          link="/mini-apps/todo"
-        />
-        <MiniAppCard
-          title="Preview Url"
-          body="When you paste url, it'll show ss."
-          image="/images/mini-apps/screenshot.gif"
-          link="/mini-apps/screenshot-url"
-        />
-        <MiniAppCard
-          title="Pomodoro"
-          body="Timer for study or progress."
-          image="/images/mini-apps/pomodoro.gif"
-          link="/mini-apps/pomodoro"
-        />
-        <MiniAppCard
-          title="Coming Soon"
-          body="Stay tuned for the next mini-apps."
-          image="/images/niwi-blog.png"
-          link="/mini-apps"
-        />
+        {list.map((item) => (
+          <MiniAppCard
+            title={item.title}
+            body={item.body}
+            image={item.image}
+            link={item.link}
+          />
+        ))}
       </div>
     </section>
   );
